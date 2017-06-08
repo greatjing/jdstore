769be868871ad5ac4ba626634ec56b6455e359e3
郭晶晶 <guojingjing@AT4280.corp.autohome.inc>
2017-06-08 17:19:57 +0800

    add like car people number

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,11 +4,16 @@ class Product < ApplicationRecord
   belongs_to :category
   belongs_to :users
 
+  #收藏
   has_many :product_relationships
   has_many :members, through: :product_relationships, source: :user
 
+  #喜欢
   has_many :like_products
   has_many :liker, through: :like_products, source: :user
 
+  def liker_number
+    liker.length
+  end
 
 end
--- app/models/product_relationship.rb
+++ app/models/product_relationship.rb
@@ -1,4 +1,5 @@
 class ProductRelationship < ApplicationRecord
   belongs_to :product
   belongs_to :user
+
 end
